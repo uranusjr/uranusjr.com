9a5c932ae8d2f7f1815df26550617918e8dcaef2
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-27 17:54:54 +0800

    Django Debug Toolbar

--- __/settings/local_uranusjr.py
+++ __/settings/local_uranusjr.py
@@ -11,3 +11,7 @@ SECRET_KEY = 'i=sp#p$s^dwm6etgercjjo4$0t77#^=!t6v4sb61ad+^tcy@)i'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)

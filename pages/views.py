34bb8b7f1edf1ebf171c698ebb1c16d7aaa9acfe
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-27 23:40:51 +0800

    Alternative template lookup implementation
    This should help performance for deep-nested pages. Not really much difference for my use really.

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,8 @@
 
 from __future__ import unicode_literals
 from django.http import Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import find_template
 from django.shortcuts import render
 from .models import Page
 
@@ -12,10 +14,18 @@ def page(request, slug, template='pages/page.html'):
         page = Page.objects.published().get(slug=slug)
     except Page.DoesNotExist:
         raise Http404
+
+    # Find the template to use by traversing the parenting chain. If nobody has
+    # a custom template, use the default 'pages/page.html'
     parent = page
-    templates = []
     while parent:
-        templates.append('pages/{slug}.html'.format(slug=parent.slug))
-        parent = parent.parent
-    templates.append(template)
-    return render(request, templates, {'page': page})
+        template_candidate = 'pages/{slug}.html'.format(slug=parent.slug)
+        try:
+            find_template(template_candidate)
+        except TemplateDoesNotExist:
+            parent = parent.parent
+        else:
+            template = template_candidate
+            break
+
+    return render(request, template, {'page': page})

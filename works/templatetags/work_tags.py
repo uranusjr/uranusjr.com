9e3d545e20ba43d11413f73f0922e4c606ce36c1
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-27 23:40:45 +0800

    Reduce queries by prefetch_related

--- works/templatetags/work_tags.py
+++ works/templatetags/work_tags.py
@@ -11,5 +11,6 @@ register = template.Library()
 @register.inclusion_tag('works/includes/sidebar.html')
 def work_sidebar():
     tags = Tag.objects.filter(works__isnull=False).distinct()
-    works = Work.objects.public().select_related('category', 'tags')
+    works = Work.objects.public()
+    works = works.select_related('category').prefetch_related('tags')
     return {'tags': tags, 'works': works}

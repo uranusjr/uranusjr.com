4cd735d83a2fb82e61cb304ae426192457c1d0b0
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-27 17:55:28 +0800

    Use select_related to reduce queries

--- works/templatetags/work_tags.py
+++ works/templatetags/work_tags.py
@@ -11,5 +11,5 @@ register = template.Library()
 @register.inclusion_tag('works/includes/sidebar.html')
 def work_sidebar():
     tags = Tag.objects.filter(works__isnull=False).distinct()
-    works = Work.objects.public()
+    works = Work.objects.public().select_related('category', 'tags')
     return {'tags': tags, 'works': works}

84506f20f65ff6174398b81f518e4ad290abdf72
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-30 01:04:59 +0800

    Move context processor module to base

--- __/settings/base.py
+++ __/settings/base.py
@@ -59,7 +59,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
-    '__.context_processors.static',
+    'base.context_processors.static',
 )
 
 ROOT_URLCONF = '__.urls'

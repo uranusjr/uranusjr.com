c7fa26b4aa3d205cbd0e8576a39652db614c1111
Tzu-ping Chung <uranusjr@gmail.com>
2014-01-01 00:30:39 +0800

    Updated deploy settings

--- __/settings/deploy.py
+++ __/settings/deploy.py
@@ -3,13 +3,14 @@
 
 from .base import *     # NOQA
 
+ALLOWED_HOSTS = ['uranusjr.com']
 
 SECRET_KEY = get_env_var('SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'uranusjr',
+        'NAME': get_env_var('DB_DEFAULT_NAME'),
         'USER': get_env_var('DB_DEFAULT_LOGIN'),
         'PASSWORD': get_env_var('DB_DEFAULT_PASSWORD'),
         'HOST': get_env_var('DB_DEFAULT_HOST'),
@@ -17,6 +18,6 @@ DATABASES = {
     },
 }
 
-STATIC_ROOT = ''
+STATIC_ROOT = get_env_var('STATIC_ROOT')
 
-MEDIA_ROOT = ''
+MEDIA_ROOT = get_env_var('MEDIA_ROOT')

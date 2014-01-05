80f7214c1312acaf92ba36c4619032ad1962bc54
Tzu-ping Chung <uranusjr@gmail.com>
2014-01-05 22:24:00 +0800

    Rewrite extra path tags to take better arguments

--- base/templatetags/base_tags.py
+++ base/templatetags/base_tags.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from django import template
+from django.template import (
+    Library, Context, Node, Variable, TemplateSyntaxError
+)
+from django.template.loader import get_template
 from base.models import ExtraPath
 from pages.models import Page
 
-register = template.Library()
+register = Library()
 
 
 @register.inclusion_tag('base/includes/tabbar.html', takes_context=True)
@@ -35,15 +38,61 @@ def sidebar_tabs(context):
     return context
 
 
-@register.inclusion_tag('base/includes/stylesheets.html')
-def stylesheets(obj, link_type):
-    files = ExtraPath.objects.filter(file_type='css', link_type=link_type)
-    return {'files': files}
+class ExtraPathNode(Node):
+    def __init__(self, file_type, link_type, obj, template_name):
+        self.file_type = file_type
+        self.link_type = link_type
+        self.obj = obj
+        self.template_name = template_name
+
+    def render(self, context):
+        owner = Variable(self.obj).resolve(context)
+        files = ExtraPath.objects.filter(
+            owner_id=owner.pk, file_type=self.file_type,
+            link_type=self.link_type,
+        )
+        template = get_template(self.template_name)
+        return template.render(Context({'files': files}))
 
 
-@register.inclusion_tag('base/includes/javascripts.html')
-def javascripts(obj, link_type):
-    files = ExtraPath.objects.filter(
-        file_type='javascript', link_type=link_type
+def _parse_extra_path(token):
+    tokens = token.split_contents()
+    tag_name = tokens.pop(0)
+    if len(tokens) != 2:
+        raise TemplateSyntaxError(
+            'Template tag {name} takes exactly two arguments.'.format(
+                name=tag_name
+            )
+        )
+    link_types = {
+        'header': ExtraPath.LINK_TYPE_HEADER,
+        'footer': ExtraPath.LINK_TYPE_FOOTER,
+    }
+    try:
+        key = tokens.pop(0).lower()
+        link_type = link_types[key]
+    except KeyError:
+        raise TemplateSyntaxError(
+            'First argument of template tag {name} should not be {key}, but '
+            'one of the followings: {candidates}.'.format(
+                name=tag_name, key=key,
+                candidates=', '.join(link_types.keys()),
+            )
+        )
+    return tokens[0], link_type
+
+
+@register.tag
+def stylesheets(parser, token):
+    obj, link_type = _parse_extra_path(token)
+    return ExtraPathNode(
+        'css', link_type, obj, 'base/includes/stylesheets.html'
     )
-    return {'files': files}
+
+
+@register.tag
+def javascripts(parser, token):
+    obj, link_type = _parse_extra_path(token)
+    return ExtraPathNode(
+        'javascript', link_type, obj, 'base/includes/javascripts.html'
+    )

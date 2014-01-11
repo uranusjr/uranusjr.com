cbeed197d13073ef5eab031a0d3d314bd68bd294
Tzu-ping Chung <uranusjr@gmail.com>
2014-01-12 03:16:32 +0800

    Parent link in admin should go to admin page

--- pages/admin.py
+++ pages/admin.py
@@ -2,30 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from base.admin import DisplayableAdmin, view_in_site
 from .models import Page
 
 
-def _page_parent_link(obj):
+def _page_parent_admin_link(obj):
     parent = obj.parent
     if parent is None:
         return _('--')
     return '<a href="{url}">{title}</a>'.format(
-        url=parent.get_absolute_url(),
-        title=parent.title
+        url=reverse('admin:pages_page_change', args=(parent.id,)),
+        title=parent.title,
     )
 
-_page_parent_link.short_description = _('parent')
-_page_parent_link.allow_tags = True
+_page_parent_admin_link.short_description = _('parent')
+_page_parent_admin_link.allow_tags = True
 
 
 class PageAdmin(DisplayableAdmin):
 
     list_display = (
         'title', 'short_description', 'state', 'published_at',
-        _page_parent_link, 'order', view_in_site
+        _page_parent_admin_link, 'order', view_in_site
     )
     list_editable = ('state', 'published_at', 'order')
     fieldsets = (

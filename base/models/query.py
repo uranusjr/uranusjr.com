578f4294a8cc4ee22144540c2c4418bc5bc89d77
Tzu-ping Chung <uranusjr@gmail.com>
2014-01-16 17:17:56 +0800

    Add some docs to note the dependency

--- base/models/query.py
+++ base/models/query.py
@@ -6,6 +6,12 @@ from django.utils.timezone import now
 
 
 class DisplayableQuerySet(QuerySet):
+    """Custom QuerySet subclass for Displayable descendants
+
+    This class requires the model using it implementing at least two fields,
+    ``state`` and ``published_at`` (a DateTimeField), and two extra attributes
+    ``STATE_PUBLIC`` and ``STATE_PRIVATE``.
+    """
     def published(self):
         return self.filter(
             state__in=(self.model.STATE_PUBLIC, self.model.STATE_PRIVATE),

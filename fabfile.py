f9b08b58ed4c08647d7212f66c44544356678592
Tzu-ping Chung <uranusjr@gmail.com>
2016-03-03 18:45:47 +0800

    Fix deploy script

--- fabfile.py
+++ fabfile.py
@@ -19,7 +19,9 @@ except AttributeError:  # Python 3 renames SafeConfigParser to ConfigParser
 
 def get_uwsgi_config(key):
     if not hasattr(get_uwsgi_config, '_config'):
-        ini_path = os.path.join(os.path.dirname(__file__), 'uwsgi.ini')
+        ini_path = os.path.join(
+            os.path.dirname(__file__), '__', 'deploy', 'uwsgi.ini',
+        )
         get_uwsgi_config._config = ConfigParser()
         get_uwsgi_config._config.read(ini_path)
     return get_uwsgi_config._config.get('uwsgi', key)
